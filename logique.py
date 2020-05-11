0fbdc26d4b80a4e79b56fd4805ae28f64ac1be6b
Eric Lung <lungszehoeric@outlook.com>
2020-05-12 00:20:23 +0200

    pays ajouter

--- logique.py
+++ logique.py
@@ -16,6 +16,21 @@ def nom ():
     
     return list_nom
 
+# nom
+def pays():
+    with open('liste_projet/pays.txt','r') as file:
+        fichier_nom = file.read()
+        liste_nom = fichier_nom.split("\n") 
+        liste = list(liste_nom)
+    pays = random.choices(liste,k=10)
+    
+    list_pays = list()
+    
+    for i in pays: 
+        list_pays.append(i)
+    
+    return list_pays
+
 # fonction pour age
 
 def age():
@@ -23,7 +38,6 @@ def age():
     random.shuffle(age)
     l_age = random.choices(age,k=10)
     list_age = list()
-
     for i_a in l_age:
         list_age.append(i_a) 
     return list_age
@@ -40,12 +54,14 @@ def sexe():
     return liste_sexe
 
 # def dictionnaire_personne():
+
 cles=["nom"]
 sexe =sexe()
 age =age()
-nom =nom()  
+nom =nom()
+pays = pays()  
 liste_p = dict()
-for (n,a,s) in itertools.zip_longest(nom, age, sexe): 
-    liste_p.update({"nom":n, "age":a, "sexe":s}) 
+for (n,a,s,p) in itertools.zip_longest(nom, age, sexe,pays): 
+    liste_p.update({"nom":n, "age":a, "sexe":s,"pays":p}) 
 
 print(liste_p)
